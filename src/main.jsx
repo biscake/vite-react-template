00172055b19145c78cae6a6523ae1565e245ab13
biscake <leonut.ljh@gmail.com>
2024-06-29 16:27:15 +0800

    Added: react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
